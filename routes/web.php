8e68a472561b9520a97d760f01cf9c4aae77c180
minnwu-1205 <nhuminh12005@gmail.com>
2026-04-21 14:35:00 +0700

    Mnhu

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,16 @@
 
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MnhuController;
 
 Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect('/');           
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 
 
 require __DIR__.'/auth.php';
+
+Route::match(['GET', 'POST'], '/timkiem', [MnhuController::class, 'search']);

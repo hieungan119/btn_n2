e819255709bbfedea2e70f9be015a4c9c73bc991
Huỳnh Thị Quỳnh Như <huynhthiquynhnhu56@gmail.com>
2026-04-21 14:47:28 +0700

    Quỳnh Như

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ManagementController;
 
 Route::get('/', [HomeController::class, 'index']);
 
@@ -12,3 +13,6 @@ Route::get('/dashboard', function () {
 
 
 require __DIR__.'/auth.php';
+Route::get('/laptop/list', [ManagementController::class, 'list_laptop'])->name('laptop.list');
+
+Route::post('/laptop/delete/{id}', [ManagementController::class, 'delete_laptop'])->name('laptop.delete');

5cdaa0e83ab937d5cc5df24fd5c77052620811df
hieungan119 <030239230139@st.buh.edu.vn>
2026-04-21 14:45:40 +0700

    ngan

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,18 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
+Route::post('/cart/delete','App\Http\Controllers\OrderController@cartdelete')->name('cartdelete');
+Route::get('/gio-hang','App\Http\Controllers\OrderController@order')->name('order');
+Route::post('/order/create','App\Http\Controllers\OrderController@ordercreate') ->middleware('auth')->name('ordercreate');
+Route::get('/testemail','App\Http\Controllers\OrderController@testemail');
+
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    //return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 
